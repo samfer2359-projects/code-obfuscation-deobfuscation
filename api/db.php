f95218f41f3138ac436fd31f63cf24234cd6168f
Samantha <samfer2359@gmail.com>
2025-09-09 21:30:25 +0530

    php file for database connection

--- api/db.php
+++ api/db.php
@@ -1,3 +1,23 @@
 <?php
-echo "<br><br><br><br>hello";
+
+// Database connection parameters
+$host = " ";    // host ip address
+$posrt = "5432";   // default postgresql port
+$dbname = " ";    // name of database
+$user = "postgres";   
+$password = " ";    // postgresql password
+
+// Create connection
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+// check if connection failed
+if(!$conn)
+{
+    echo "Error : Unable to connect to the database.";
+    exit;
+}
+
+// if connection is successful, the connection object (link) is established
+echo "Connected to the database successfully!";
+
 ?>
